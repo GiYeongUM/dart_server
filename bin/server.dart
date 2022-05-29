51614dc03a7cd9503c94c034f4f00c781100bb64
GiYeong Um <eomky2005@gmail.com>
2022-05-29 15:07:30 +0900

    [update] 분석용 예제 업데이트

--- bin/server.dart
+++ bin/server.dart
@@ -1,32 +1,110 @@
+import 'dart:convert';
 import 'dart:io';
 
-import 'package:shelf/shelf.dart';
-import 'package:shelf/shelf_io.dart';
-import 'package:shelf_router/shelf_router.dart';
-
-// Configure routes.
-final _router = Router()
-  ..get('/', _rootHandler)
-  ..get('/echo/<message>', _echoHandler);
-
-Response _rootHandler(Request req) {
-  return Response.ok('Hello, World!\n');
+Future<void> main() async {
+  final server = await createServer();
+  print('Server started: ${server.address} port ${server.port}');
+  await handleRequests(server);
 }
 
-Response _echoHandler(Request request) {
-  final message = params(request, 'message');
-  return Response.ok('$message\n');
+Future<HttpServer> createServer() async {
+  final address = InternetAddress.loopbackIPv4;
+  const port = 4040;
+  return await HttpServer.bind(address, port);
 }
 
-void main(List<String> args) async {
-  // Use any available host or container IP (usually `0.0.0.0`).
-  final ip = InternetAddress.anyIPv4;
+Future<void> handleRequests(HttpServer server) async {
+  await for (HttpRequest request in server) {
+    switch (request.method) {
+      case 'GET':
+        handleGet(request);
+        break;
+      case 'POST':
+        handlePost(request);
+        break;
+      default:
+        handleDefault(request);
+    }
+  }
+}
 
-  // Configure a pipeline that logs requests.
-  final _handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
+var myStringStorage = 'Hello from a Dart server';
+const fruit = ['apple', 'banana', 'peach', 'pear'];
 
-  // For running in containers, we respect the PORT environment variable.
-  final port = int.parse(Platform.environment['PORT'] ?? '8080');
-  final server = await serve(_handler, ip, port);
-  print('Server listening on port ${server.port}');
+/// GET requests
+void handleGet(HttpRequest request) {
+  final path = request.uri.path;
+  switch (path) {
+    case '/fruit':
+      handleGetFruit(request);
+      break;
+    case '/vegetables':
+      handleGetVegetables(request);
+      break;
+    default:
+      handleGetOther(request);
+  }
+}
+
+void handleGetFruit(HttpRequest request) {
+  final queryParams = request.uri.queryParameters;
+
+  // Return all fruit if there are no query parameters
+  if (queryParams.isEmpty) {
+    final jsonString = jsonEncode(fruit);
+    request.response
+      ..statusCode = HttpStatus.ok
+      ..write(jsonString)
+      ..close();
+    return;
+  }
+
+  // Find any fruit that start with the 'prefix'
+  final prefix = queryParams['prefix'];
+  final matches = fruit
+      .where(
+        (item) => item.startsWith(prefix),
+  ).toList();
+
+  // Respond based on the matches found
+  if (matches.isEmpty) {
+    request.response
+      ..statusCode = HttpStatus.notFound
+      ..close();
+  } else {
+    final jsonString = jsonEncode(matches);
+    request.response
+      ..statusCode = HttpStatus.ok
+      ..write(jsonString)
+      ..close();
+  }
+}
+
+void handleGetVegetables(HttpRequest request) {
+  request.response
+    ..statusCode = HttpStatus.ok
+    ..write(myStringStorage)
+    ..close();
+}
+
+void handleGetOther(HttpRequest request) {
+  request.response
+    ..statusCode = HttpStatus.badRequest
+    ..close();
+}
+
+/// POST requests
+Future<void> handlePost(HttpRequest request) async {
+  myStringStorage = await utf8.decoder.bind(request).join();
+  request.response
+    ..write('Got it. Thanks.')
+    ..close();
+}
+
+/// Other HTTP method requests
+void handleDefault(HttpRequest request) {
+  request.response
+    ..statusCode = HttpStatus.methodNotAllowed
+    ..write('Unsupported request: ${request.method}.')
+    ..close();
 }
